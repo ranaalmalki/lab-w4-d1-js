788b740f6a08c3c7a6d172d637ef49108fd58045
Rana Almalki <ranaalmalki12@gmail.com>
2024-09-15 10:41:29 +0300

    solve bacground color

--- main.js
+++ main.js
@@ -6,11 +6,11 @@ let back = document.getElementById("sequer")
 let backv = back
 let btn = document.getElementById("btn-back")
 document.getElementById("btn-click").addEventListener("click", ()=>{
-    // let inputbg  = document.getElementById("input-bg-color").value
+    let inputbg  = document.getElementById("input-bg-color").value
     let inputwidth  = document.getElementById("input-width").value
 let inputheight = document.getElementById("input-height").value
 let inputborder = document.getElementById("input-border").value
-// document.getElementById("sequer").style.backgroundColor=`${inputbg}`
+document.getElementById("sequer").style.backgroundColor=`${inputbg}`
 document.getElementById("sequer").style.height=`${inputwidth}%`
 document.getElementById("sequer").style.width= `${inputheight}%`
 document.getElementById("sequer").style.borderRadius= `${inputborder}rem`
@@ -19,6 +19,6 @@ document.getElementById("sequer").style.borderRadius= `${inputborder}rem`
 
 
 btn.addEventListener("click",()=>{
-    
+    backv
 })
 
